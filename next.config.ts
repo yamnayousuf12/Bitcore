f6af8c5f2e6de477d8dbcfa5161f8ec905de9f0d
yamnayousuf12 <154628218+yamnayousuf12@users.noreply.github.com>
2025-08-30 20:44:17 +0500

    Changes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
+
+
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
