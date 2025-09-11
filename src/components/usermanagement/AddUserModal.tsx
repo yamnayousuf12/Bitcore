6ced25c65702a772859bd9227890026b6aaf8e9a
yamnayousuf12 <154628218+yamnayousuf12@users.noreply.github.com>
2025-09-12 01:22:15 +0500

    Changes

--- src/components/usermanagement/AddUserModal.tsx
+++ src/components/usermanagement/AddUserModal.tsx
@@ -311,9 +311,9 @@ export default function AddUserModal({
     "w-full h-11 rounded-2xl bg-white/5 border border-white px-4 text-white placeholder:text-white/50 focus:outline-none";
 
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-[#0b1220] p-4 md:p-8">
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-[#0b1220] p-4 md:p-8 mt-28 md:mt-0">
       {/* frame */}
-      <div className="p-1 rounded-2xl w-full max-w-4xl bg-gradient-to-b from-white/[0.08] to-white/[0.03] mt-28">
+      <div className="p-1 rounded-2xl w-full max-w-4xl bg-gradient-to-b from-white/[0.08] to-white/[0.03] ">
         {/* card */}
         <div
           className="relative rounded-2xl border-2 border-dotted border-white"
