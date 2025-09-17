d273ece1f6601c0e3a87c9a3209fa657bfea7dc2
yamnayousuf12 <154628218+yamnayousuf12@users.noreply.github.com>
2025-09-18 00:13:33 +0500

    Changes

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -367,8 +367,8 @@ export default function DashboardPage() {
         <Image
           src={DashboardImages.firsticon}
           alt="Total Users"
-          width={50}
-          height={50}
+          width={70}
+          height={70}
           className="object-contain"
         />
       }
@@ -382,8 +382,8 @@ export default function DashboardPage() {
         <Image
           src={DashboardImages.secondicon}
           alt="Pending Tasks"
-          width={50}
-          height={50}
+          width={70}
+          height={70}
           className="object-contain"
         />
       }
@@ -397,8 +397,8 @@ export default function DashboardPage() {
         <Image
           src={DashboardImages.thirdicon}
           alt="Complete Tasks"
-          width={50}
-          height={50}
+          width={70}
+          height={70}
           className="object-contain"
         />
       }
@@ -412,8 +412,8 @@ export default function DashboardPage() {
         <Image
           src={DashboardImages.fourthicon}
           alt="Average Hours"
-          width={50}
-          height={50}
+          width={70}
+          height={70}
           className="object-contain"
         />
       }
