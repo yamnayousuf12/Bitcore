28e43154ee4353eca7ddb936983250cba500372e
yamnayousuf12 <154628218+yamnayousuf12@users.noreply.github.com>
2025-09-26 17:29:17 +0500

    Changes

--- src/app/(dashboard)/dashboard/usermanagement/page.tsx
+++ src/app/(dashboard)/dashboard/usermanagement/page.tsx
@@ -224,13 +224,17 @@ export default function UserManagement() {
   <span className="font-medium tracking-wide">User Management</span>
 </div>
       {/* Search + Add */}
-      <div className="flex gap-3 mb-4 md:h-auto h-12 -mt-8 md:mt-0 items-center justify-center md:justify-start md:items-start">
+      <div className="flex gap-3 mb-4 md:h-auto h-12 -mt-8 md:mt-0 items-end justify-end md:justify-start md:items-start ">
         <input
           type="text"
           placeholder="Search User..."
           value={search}
           onChange={(e) => setSearch(e.target.value)}
-          className="px-4 py-2 rounded-xl bg-gray3 border border-gray/40 focus:outline-none md:w-96 w-full "
+          className="px-4 py-2 rounded-xl bg-gray3 border border-gray/40 focus:outline-none w-[300px] sm:w-72  
+    md:w-72  
+    lg:w-72 
+    xl:w-72 
+    2xl:w-72 "
         />
         <button
           className="bg-Blue md:px-4 px-3 py-2 rounded-xl hover:bg-Blue flex-shrink-0"
